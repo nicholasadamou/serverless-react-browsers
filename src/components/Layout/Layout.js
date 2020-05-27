9d9b93657db1ae8a7f8d5011c3e21031a2824867
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-27 08:20:35 -0400

    added react-helmet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "animate.css/animate.min.css";
+import { Helmet } from "react-helmet";
 
 import GlobalStyles from "../../sass/GlobalStyles";
 
@@ -10,6 +10,26 @@ const Layout = (content) => (
 	<>
 		<GlobalStyles />
 		<Fonts />
+		<Helmet htmlAttributes={{ lang: "en" }} title="Serverless React Browsers">
+			<link rel="icon" href="/assets/favicons/favicon.ico" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/assets/favicons/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/assets/favicons/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/assets/favicons/favicon-16x16.png"
+			/>
+		</Helmet>
 		{content()}
 	</>
 );
